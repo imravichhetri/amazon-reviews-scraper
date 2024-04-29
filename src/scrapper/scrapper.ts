4d80774b7b1ba1504cec3b8dc4c8ddf8e199ad7e
Ravindra Chhetri <ravindra.chhetri@ip-192-168-1-16.eu-central-1.compute.internal>
2024-04-29 21:38:53 +0200

    MINOR: basic scrapping function done

--- src/scrapper/scrapper.ts
+++ src/scrapper/scrapper.ts
@@ -2,7 +2,7 @@
 
 import puppeteer from 'puppeteer'
 import * as cheerio from 'cheerio'
-import { last } from 'cheerio/lib/api/traversing'
+import fs from 'fs'
 
 const getReviews = async (url: string) => {
   // The first two elements of process.argv are the Node.js executable path and the path to the script file
@@ -38,11 +38,47 @@ const getReviews = async (url: string) => {
     console.log(isNextButtonDisabled, 'isNextButtonDisabled')
     while (!isNextButtonDisabled) {
       console.info(isNextButtonDisabled, 'isNextButtonDisabled')
+      await page.waitForSelector('.reviews-loading', { hidden: true })
+      // const result = await page.content()
       const reviewDivs = await page.$$('[data-hook="review"]')
       // const profileDivs = await page.$$(
       //   'div.a-profile-content>span.a-profile-name'
       // )
       reviewDivList.push(...reviewDivs)
+      // Parse the HTML using Cheerio
+      // const $ = cheerio.load(result);
+      // const $ = cheerio.load(await page.content())
+      //   const $ = cheerio.load(`
+      //   <ul>
+      //     <li>One</li>
+      //     <li>Two</li>
+      //     <li class="blue sel">Three</li>
+      //     <li class="red">Four</li>
+      //   </ul>
+      // `)
+      //   const data = $.extract({
+      //     releases: [
+      //       {
+      //         // First, we select individual release sections.
+      //         selector: 'section',
+      //         // Then, we extract the release date, name, and notes from each section.
+      //         value: {
+      //           // Selectors are executed whitin the context of the selected element.
+      //           name: 'h2',
+      //           date: {
+      //             selector: 'relative-time',
+      //             // The actual date of the release is stored in the `datetime` attribute.
+      //             value: 'datetime',
+      //           },
+      //           notes: {
+      //             selector: '.markdown-body',
+      //             // We are looking for the HTML content of the element.
+      //             value: 'innerHTML',
+      //           },
+      //         },
+      //       },
+      //     ],
+      //   })
       const reviewHTML = await page.$$eval(
         '[data-hook="review"] div.a-profile-content',
         (elems) => elems.map((elem) => elem.innerHTML)
@@ -86,21 +122,44 @@ const getReviews = async (url: string) => {
         isNextButtonDisabled,
         'nextButton'
       )
+      await page.screenshot({ path: 'screenshot.png' })
 
       if (!isNextButtonDisabled) {
+        // await Promise.all([
         await page.click('.a-last>a:first-child')
-        const url = await page.evaluate(async () => {
-          const anchor = document.querySelector('.a-last > a:first-child')
-          return (anchor as any)?.href
-        })
-        console.log(url, 'url-----')
-        await page.waitForResponse((response) => {
-          if (response.url().includes('https://www.amazon.de/hz/')) {
-            console.log(response.url(), '----')
-          }
-          return response.url().includes('/reviews-render/ajax/reviews/')
-        })
+        // const url = await page.evaluate(async () => {
+        //   const anchor = document.querySelector('.a-last > a:first-child')
+        //   return (anchor as any)?.href
+        // })
+        // console.log(url, 'url-----')
+
+        // await page.waitForSelector('div.reviews-loading') // Adjust wait time as needed
+        // await page.waitForResponse((response) => {
+        //   if (response.url().includes('https://www.amazon.de/hz/')) {
+        //     console.log(response.url(), '----')
+        //   }
+        //   return response.url().includes('/reviews-render/ajax/reviews/')
+        // })
+
+        await page.waitForFunction(
+          () => {
+            const loadingElements = document.querySelector(
+              'div.reviews-loading'
+            )
+            console.log(loadingElements, 'loadingElements')
+            return loadingElements !== null
+          },
+          { timeout: 10000 }
+        )
+        // page.waitForSelector('[data-hook="review"] .a-profile-name'),
         // await page.waitForSelector('[data-hook="review"] div.a-profile-content') // Adjust wait time as needed
+        // ])
+        const reviewHTML = await page.$eval(
+          '[data-hook="review"] div.a-profile-content',
+          (elem) => elem.innerHTML
+        )
+
+        console.log(reviewHTML, 'after wait')
       }
     }
 
